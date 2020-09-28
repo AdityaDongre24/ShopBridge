530f1c81a32fb76bc03cb417fcc3af0996adf4c2
AdityaDongre24 <58044425+AdityaDongre24@users.noreply.github.com>
2020-09-28 10:18:08 +0530

    Add files via upload

---
+++ ShopBridge/Shared/AlertModalBase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopBridge.Shared
+{
+    public class AlertModalBase : ComponentBase
+    {
+        public bool ShowAlert { get; set; } = false;
+
+        [Parameter]
+        public string ModalHeader { get; set; } = "Alert";
+        [Parameter]
+        public string ModalBody { get; set; } = "Operation Performed Successfully";
+        [Parameter]
+        public string ModalButtonText { get; set; } = "OK";
+        [Parameter]
+        public EventCallback OnAlertClicked { get; set; }
+
+        public void Show()
+        {
+            ShowAlert = true;
+            StateHasChanged();
+        }
+
+        protected async Task CloseAlert()
+        {
+            ShowAlert = false;
+            await OnAlertClicked.InvokeAsync(null);
+        }
+    }
+}
---
+++ ShopBridge/Shared/ConfirmModalBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopBridge.Shared
+{
+    public class ConfirmModalBase : ComponentBase
+    {
+        protected bool ShowConfirmation { get; set; } = false;
+        [Parameter]
+        public EventCallback<bool> ConfirmationChanged { get; set; }
+
+        public void Show()
+        {
+            ShowConfirmation = true;
+            StateHasChanged();
+        }
+
+        protected async Task OnConfirmationChanged(bool value)
+        {
+            ShowConfirmation = false;
+            await ConfirmationChanged.InvokeAsync(value);
+        }
+    }
+}
