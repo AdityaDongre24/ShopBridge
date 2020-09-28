b44ea03bf846087a58b96284f2fab569aa26fc34
AdityaDongre24 <58044425+AdityaDongre24@users.noreply.github.com>
2020-09-28 10:04:24 +0530

    ShopBridge_Model

---
+++ ShopBridgeModels/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopBridgeModels
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public float Price { get; set; }
+
+        public string ImageSource { get; set; }
+    }
+}
